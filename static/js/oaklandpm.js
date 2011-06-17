566c0da8e7373a7fbf1948e8894de2d332d4bf2e
Chris Yap <buzzyapyear@gmail.com>
2011-06-17 14:14:35 -0700

    setting up main JS controller

--- static/js/oaklandpm.js
+++ static/js/oaklandpm.js
@@ -14,12 +14,39 @@ opm.OaklandPm = function (options) {
     };
     $.extend(this.options, options);
     
-    this.handleSearchForm();
+    this.vars = {
+        'current_page': $('#home')
+    };
+    
+    this.setupPage('home');
     
 }
 
 opm.OaklandPm.prototype = {
-    handleSearchForm: function() {
+    setupPage: function(page_id) {
+        this.vars.current_page = $(['#', page_id].join(''));
+        var page_id_split = page_id.split("-");
+        var method_name = [];
+        $.each(page_id_split, function(i, item) {
+            item = item.split('');
+            item[0] = item[0].toUpperCase();
+            item = item.join('');
+            method_name.push(item);
+        });
+        method_name = ['handle', method_name.join('')].join('');
+        this[method_name]();
+    },
+    
+    handlePageTransition: function(destination) {
+        $(this.vars.current_page).fadeOut('fast', function() {
+            var next_page = $(this.vars.current_page).next('li.page');
+            $(next_page).fadeIn('slow');
+            window.scroll(0,0);
+            this.setupPage($(next_page).attr('id'));
+        }.bindScope(this));
+    },
+    
+    handleHome: function() {
         
     }
 }
