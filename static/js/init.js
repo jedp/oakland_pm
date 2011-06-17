ce8b9cfa010864ec80e5debf2d5c692d4bfb734d
Chris Yap <buzzyapyear@gmail.com>
2011-06-17 12:37:00 -0700

    adding init js

---
+++ static/js/init.js
@@ -0,0 +1,20 @@
+/*
+ * Oakland PM js init
+ * Also, bingo bango.
+ * @author cyap
+ */
+ 
+
+// create an oaklandPM namespace for all our js assets
+var opm = {
+    'constants': {},
+    'common': {}
+};
+
+// define api environment
+opm.constants.API_BASE_URL = '/api/?';
+
+// define common functions
+opm.common.apicall = function (path, params, callback) {
+    $.postJSON([line2.constants.API_BASE_URL, path].join(''), params, callback);
+}
