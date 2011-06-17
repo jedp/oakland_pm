87c9158983007a6171e127b70aafff49b3307e34
Chris Yap <buzzyapyear@gmail.com>
2011-06-17 14:02:49 -0700

    setting up main JS controller

--- static/js/oaklandpm.js
+++ static/js/oaklandpm.js
@@ -1,1 +1,25 @@
-// hello, peeps
+
+/*
+ * OaklandPm Class
+ * Main JS controller
+ * Also, black border?  Adonis DNA.
+ * @author cyap
+ */
+
+opm.OaklandPm = function (options) {
+    
+    // init options
+    this.options = {
+
+    };
+    $.extend(this.options, options);
+    
+    this.handleSearchForm();
+    
+}
+
+opm.OaklandPm.prototype = {
+    handleSearchForm: function() {
+        
+    }
+}
