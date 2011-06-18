93d49f03be1095934d79b5d9247dddb780a4fe56
Jed Parsons <jedp@me.com>
2011-06-18 16:05:47 -0700

    re-introduced csrf

--- web/views.py
+++ web/views.py
@@ -3,36 +3,43 @@ from django.template import RequestContext
 from django.shortcuts import render_to_response
 from django.core.context_processors import csrf
 
-def home(request):
-    return render_to_response('home.html', 
-            context_instance=RequestContext(request))
-    
-def feed(request):
-    return render_to_response('feed.html', 
-            {'events': ['foo']},
-            context_instance=RequestContext(request))
-
-def search(request):
-    return render_to_response('search.html',
-            context_instance=RequestContext(request))
-
-def friends(request):
-    return render_to_response('friends.html',
-            context_instance=RequestContext(request))
-
-def messages(request):
-    return render_to_response('messages.html',
-            context_instance=RequestContext(request))
-
-def event(request, event_id):
-    return render_to_response('event.html', 
-            context_instance=RequestContext(request))
-
-def categories(request):
-    return render_to_response('categories.html', 
-            context_instance=RequestContext(request))
-
 def get_csrf(request):
     c = {}
     c.update(csrf(request))    
     return c
+
+def home(request):
+    return render_to_response('home.html', 
+            {'c': get_csrf(request)},
+            context_instance=RequestContext(request))
+    
+def feed(request):
+    return render_to_response('feed.html', 
+            {'events': ['foo'],
+             'c': get_csrf(request)},
+            context_instance=RequestContext(request))
+
+def search(request):
+    return render_to_response('search.html',
+            {'c': get_csrf(request)},
+            context_instance=RequestContext(request))
+
+def friends(request):
+    return render_to_response('friends.html',
+            {'c': get_csrf(request)},
+            context_instance=RequestContext(request))
+
+def messages(request):
+    return render_to_response('messages.html',
+            {'c': get_csrf(request)},
+            context_instance=RequestContext(request))
+
+def event(request, event_id):
+    return render_to_response('event.html', 
+            {'c': get_csrf(request)},
+            context_instance=RequestContext(request))
+
+def categories(request):
+    return render_to_response('categories.html', 
+            {'c': get_csrf(request)},
+            context_instance=RequestContext(request))
