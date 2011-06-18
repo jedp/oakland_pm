c81e2e7ddf3263c1f6c75f483fff30591404e409
Chris Yap <buzzyapyear@gmail.com>
2011-06-18 16:36:48 -0700

    fixiing xhr categories

--- web/xhr.py
+++ web/xhr.py
@@ -27,7 +27,7 @@ def event(request, event_id):
 def categories(request):
     c = get_csrf(request)    
     return render_to_response(
-            'categories.html', 
+            'list_events.html', 
             {"c":c},
             context_instance=RequestContext(request))
 
