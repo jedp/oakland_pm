bb5fa520923cfad374c76fe09d86774ede4660c1
Jed Parsons <jedp@me.com>
2011-06-18 16:01:33 -0700

    template tags, separate xhr views

--- web/views.py
+++ web/views.py
@@ -3,47 +3,34 @@ from django.template import RequestContext
 from django.shortcuts import render_to_response
 from django.core.context_processors import csrf
 
-def debug(request):
-    print request
-    return render_to_response(
-            'debug.html', 
-            {'request':str(request)})
-
 def home(request):
-    c = get_csrf(request)    
-    return render_to_response(
-            'base.html', 
-            {"c":c},
+    return render_to_response('home.html', 
             context_instance=RequestContext(request))
     
 def feed(request):
-    c = get_csrf(request)    
-    return render_to_response(
-            'feed.html', 
-            {"c":c},
+    return render_to_response('feed.html', 
+            {'events': ['foo']},
             context_instance=RequestContext(request))
 
-def event(request):
-    c = get_csrf(request)    
-    return render_to_response(
-            'event_detail.html', 
-            {"c":c},
+def search(request):
+    return render_to_response('search.html',
+            context_instance=RequestContext(request))
+
+def friends(request):
+    return render_to_response('friends.html',
+            context_instance=RequestContext(request))
+
+def messages(request):
+    return render_to_response('messages.html',
+            context_instance=RequestContext(request))
+
+def event(request, event_id):
+    return render_to_response('event.html', 
             context_instance=RequestContext(request))
 
 def categories(request):
-    c = get_csrf(request)    
-    return render_to_response(
-            'categories.html', 
-            {"c":c},
+    return render_to_response('categories.html', 
             context_instance=RequestContext(request))
-
-def base(request):
-    c = get_csrf(request)
-    return render_to_response(
-            'base.html', 
-            {"c":c},
-            context_instance=RequestContext(request))
-
 
 def get_csrf(request):
     c = {}
